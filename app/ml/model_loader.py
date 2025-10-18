fc73650ff512d78a4c89667fd8fb7b3cdf5470d3
Xavier COULON <69201590+XavierCoulon@users.noreply.github.com>
2025-10-18 17:59:31 +0200

    bugfix : add HF cache directory (#46)
    * bugfix : add HF cache directory
    
    * bugfix : add directory for Github

--- app/ml/model_loader.py
+++ app/ml/model_loader.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import joblib
@@ -8,6 +9,12 @@ MODEL_PATH = Path(__file__).resolve().parent / "random_forest_pipeline.pkl"
 # Nom du dépôt et fichier sur Hugging Face
 HF_REPO_ID = "XavierCoulon/futurisys-model"
 HF_FILENAME = "random_forest_pipeline.pkl"
+if os.environ.get("GITHUB_ACTIONS") == "true":
+    # En CI GitHub → dossier temporaire accessible
+    HF_DIRECTORY = Path("/tmp/.cache")
+else:
+    # En local / Docker Hugging Face
+    HF_DIRECTORY = Path("/app/.cache")
 
 logging.basicConfig(
     level=logging.INFO,
@@ -27,9 +34,12 @@ def load_model():
             logging.error(f"❌ Échec du chargement local : {e}")
     logging.info("🌐 Téléchargement du modèle depuis Hugging Face...")
     try:
+        os.makedirs(HF_DIRECTORY, exist_ok=True)
         model_path = hf_hub_download(
             repo_id=HF_REPO_ID,
             filename=HF_FILENAME,
+            cache_dir=HF_DIRECTORY,
+            force_download=True,
         )
         model = joblib.load(model_path)
         logging.info("✅ Modèle téléchargé et chargé depuis Hugging Face.")
